b5ff1d06e0a8d9a193bad277579f51c878f8e8ae
Thomas Kadauke <tkadauke@imedo.de>
2012-12-02 17:38:20 +0100

    Implemented a very simple graphical log server for use with the NetworkLogger in motion-support

--- Logger/AppDelegate.rb
+++ Logger/AppDelegate.rb
@@ -6,10 +6,53 @@
 #  Copyright 2012 Thomas Kadauke. All rights reserved.
 #
 
+require 'socket'
+
 class AppDelegate
   attr_accessor :window
-  def applicationDidFinishLaunching(a_notification)
-    # Insert code here to initialize your application
+  attr_accessor :table_view
+  
+  def awakeFromNib
+    @data = []
+    
+    @server = TCPServer.open(2000)
+    Thread.start do
+      loop do
+        Thread.start(@server.accept) do |client|
+          while line = client.gets
+            @data << [Time.now.to_s, line.chop]
+            table_view.reloadData
+          end
+          client.close
+        end
+      end
+    end
+  end
+  
+  def numberOfRowsInTableView(tableView)
+    @data.size
+  end
+  
+  def tableView(tableView, objectValueForTableColumn:column, row:row)
+    data_row = @data[row]
+    case column.identifier
+    when 'timestamp'
+      data_row[0]
+    when 'message'
+      data_row[1]
+    end
+  end
+  
+  def clear(sender)
+    @data = []
+    table_view.reloadData
+  end
+
+  def copy(sender)
+    rows_to_copy = []
+    table_view.selectedRowIndexes.enumerateIndexesUsingBlock lambda { |index, dummy| rows_to_copy << @data[index][1] }
+    pasteboard = NSPasteboard.generalPasteboard
+    changeCount = pasteboard.clearContents
+    pasteboard.writeObjects(rows_to_copy)
   end
 end
-
